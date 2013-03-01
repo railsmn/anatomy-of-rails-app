03e57675ee7d67f6a32dd630d9597057a8763534
Derek <derek.a.rockwell@gmail.com>
2013-03-01 14:46:45 -0600

    added enabled to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120826102940) do
+ActiveRecord::Schema.define(:version => 20130301204617) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20120826102940) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.boolean  "opt_in"
+    t.boolean  "enabled"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
